8bc7c852c2784f05951e1e291f6826c9bd1ba3f9
Vegard Antun <vegarant@student.matnat.uio.no>
2022-05-10 10:46:36 +0200

    Deleted many redundant files

--- adv_tools_PNAS/automap_config.py
+++ adv_tools_PNAS/automap_config.py
@@ -1,8 +1,8 @@
 # Update the paths so that they match the underlying data.
 
-src_weights        = '/home/lfi/workspace/automap_nma_response/NMA_Response_Repo/NMA_Response_Storage';
-src_data           = '/home/lfi/workspace/automap_nma_response/NMA_Response_Repo/NMA_Response_Storage';
-src_k_mask         = '/home/lfi/workspace/automap_nma_response/NMA_Response_Repo/NMA_Response_Storage';
-src_automap_runner = '/home/lfi/workspace/automap_nma_response/NMA_Response_Repo/NMA_Response_Storage/runners';
 
+src_weights        = '/mn/kadingir/vegardantun_000000/nobackup/storage_all/storage_automap_not_robust'
+src_data           = '/mn/kadingir/vegardantun_000000/nobackup/storage_all/storage_automap_not_robust'
+src_k_mask         = '/mn/kadingir/vegardantun_000000/nobackup/storage_all/storage_automap_not_robust'
+src_automap_runner = '/mn/kadingir/vegardantun_000000/nobackup/storage_all/storage_automap_not_robust/runners'
 
